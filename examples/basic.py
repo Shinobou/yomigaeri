8ff7b89a63af8b72b86a6e4e256e6934340c3c5f
Shinobou <86394469+Shinobou@users.noreply.github.com>
2022-06-11 20:56:28 -0500

    Update basic.py

--- examples/basic.py
+++ examples/basic.py
@@ -1,25 +1,25 @@
+import logging
 import os
 
 import hikari
+import yomigaeri
 
-from yomigaeri import Bot
-
-bot = Bot("!", os.environ.get("DISCORD_TOKEN"))
-
-
-@bot.command("test", "tests")
-async def command(event: hikari.MessageCreateEvent) -> None:
-    await event.message.respond("Hey")
-
-
-@bot.command("add", "adds two numbers")
-async def add(event: hikari.MessageCreateEvent, number_one: int, number_two: int) -> None:
-    await event.message.respond(number_one + number_two)
+bot: yomigaeri.Bot = yomigaeri.Bot("!", os.environ.get("DISCORD_TOKEN"))
 
 
 @bot.listen(hikari.StartedEvent)
 async def on_started_event(_: hikari.StartedEvent) -> None:
-    print("ONLINE!!!")
+    logging.info("Bot is online!")
+
+
+@bot.command("ping", "Checks the latency of the bot.")
+async def on_ping_command(event: hikari.MessageCreateEvent) -> None:
+    await event.message.respond(f"Latency: {bot._gateway_bot.heartbeat_latency * 1000:.1f} ms")
+
+
+@bot.command("add", "Adds two numbers together.")
+async def on_add_command(event: hikari.MessageCreateEvent, first_number: float, second_number: float) -> None:
+    await event.message.respond(f"{first_number} + {second_number} = {first_number + second_number}")
 
 
 bot.run()
