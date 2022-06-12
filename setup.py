42de72e49a4552bc6b3d3a18ad880d4bf3f2cc90
Shinobou <86394469+Shinobou@users.noreply.github.com>
2022-06-11 20:47:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 from setuptools import setup
 
-from mypyc.build import mypycify
-
 setup(
     name="yomigaeri",
-    packages=["yomigaeri"],
-    ext_modules=mypycify([
-        "yomigaeri/__init__.py",
-        "yomigaeri/bot.py",
-        "yomigaeri/command.py"
-    ]),
+    packages=["yomigaeri"]
     install_requires=[
-        "hikari~=2.0.0.dev108",
-        "mypy~=0.961"
+        "hikari~=2.0.0.dev108"
     ]
 )
